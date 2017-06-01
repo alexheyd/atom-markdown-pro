822b6f9622841d4139392965cb832637e41b99d8
Alex Heyd <alex@gaikai.com>
2017-06-01 15:41:42 -0700

    Initial commit.

---
+++ spec/atom-markdown-pro-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMarkdownProView from '../lib/atom-markdown-pro-view';
+
+describe('AtomMarkdownProView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
